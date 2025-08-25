2c32fa887fd170d2a9fbe87a6f8271210316361e
Akash-dev-lab <111779287+Akash-dev-lab@users.noreply.github.com>
2025-08-24 21:15:53 -0500

    Imagekit integrated and code optimized

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,18 +1,28 @@
-const { json } = require("express")
 const postModel = require("../models/post.model")
 const generateCaption = require("../services/ai.services")
+const UploadFile = require("../services/storage.services")
+const {v4: uuidv4} = require('uuid')
 
 async function createbPostController(req, res) {
     const file = req.file
     console.log("File Data: ", file)
 
-    const base64Image = Buffer.from(file.buffer).toString('base64')
+    const base64Image = new Buffer.from(file.buffer).toString('base64')
 
-    const caption = await generateCaption(base64Image)
+    // const caption = await generateCaption(base64Image)
+    // const result = await UploadFile(base64Image, `${uuidv4()}`)
 
-    res.status(200).json({
-        caption: caption
+    const [caption, result] = await Promise.all([
+        generateCaption(base64Image),
+        UploadFile(base64Image, `${uuidv4()}`)
+    ])
+
+    res.json({
+        caption: caption,
+        result
     })
  }
 
- module.exports = createbPostController
+ module.exports = {
+    createbPostController
+}
